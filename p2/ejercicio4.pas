e9bdde26c4bdcd0e9fbfe624642d5c93d8bbd370
cotykrm <coty.krm@gmail.com>
2025-04-15 00:55:17 -0300

    Add files via upload

--- p2/ejercicio4.pas
+++ p2/ejercicio4.pas
@@ -25,3 +25,114 @@ type
     precio:real;
   end;
   
+  venta = record
+    codigo:integer;
+    cv:integer;
+  end;
+  
+  maestro = file of producto;
+  detalle = file of venta;
+  detalles = array [1..30] of detalle;
+  
+procedure leerProducto(var p:producto);
+begin
+  writeln('Ingrese código del producto: '); readln(p.codigo);
+  if(p.codigo<>-1)then begin
+    writeln('Ingrese nombre: '); readln(p.nombre);  
+    writeln('Ingrese descripción: '); readln(p.descripcion);
+    writeln('Ingrese stock disponible: '); readln(p.stockD);
+    writeln('Ingrese stock mínimo: '); readln(p.stockM);
+    writeln('Ingrese precio: '); readln(p.precio);
+  end;
+end;
+
+procedure leerVenta(var v:venta);
+begin
+  writeln('Ingrese código del producto: '); readln(v.codigo);
+  if(v.codigo<>-1)then begin
+    writeln('Ingrese cantidad vendida: '); readln(v.cv);  
+  end;
+end;
+procedure crearArchivoM(var ar:maestro);
+var
+  p:producto;
+begin
+  rewrite(ar);
+  leerProducto(p);
+  while(p.codigo<>-1)do begin
+    write(ar,p);
+    leerProducto(p);
+  end;
+  close(ar);
+end;
+
+procedure crearArchivoD(var ar:detalle);
+var
+  v:venta;
+begin
+  rewrite(ar);
+  leerVenta(v);
+  while(v.codigo<>-1)do begin
+    write(ar,v);
+    leerVenta(v);
+  end;
+  close(ar);
+end;
+
+procedure armarDetalles(var d:detalles);
+var
+  i:integer;
+begin
+  for i:= 1 to 30 do
+    crearArchivoD(d[i]);
+end;
+
+procedure imprimirVenta(v:venta);
+begin
+  writeln('codigo: ',v.codigo);
+  writeln('cantidad vedida: ',v.cv);
+end;
+
+procedure imprimirDet(var ar:detalle);
+var
+  v:venta;
+begin
+  reset(ar);
+  while(not eof(ar)) do begin
+    read(ar,v);
+    imprimirVenta(v);
+  end;
+  close(ar);
+end;
+
+procedure imprimirDetalles(d:detalles);
+var
+  i:integer;
+begin
+  for i:=1 to 30 do 
+    imprimirDet(d[i]);
+end;
+
+procedure leer(var ar:detalle; var v:venta);
+begin
+  if(not eof(ar))then
+    read(ar,v)
+  else
+    v.codigo:= valorAlto;
+end;
+
+procedure minimo(d:detalles; var min:detalle);
+var
+  minC,i:integer; v:venta;
+begin
+  minC:=-1;
+  for i:=1 to 30 do begin
+    leer(d[i],v);
+    if(v.codigo<minC)then
+      minC:=i;
+  end;
+  min:=d[i];
+  leer()
+    
+
+
